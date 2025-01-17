a0fe6bf8907519153673d9e1fe7bba2378a54874
lazdinst <lazdinst@gmail.com>
2025-01-17 11:48:20 -0500

    added todo types

--- client/src/redux/slices/todo/types/index.ts
+++ client/src/redux/slices/todo/types/index.ts
@@ -1,3 +1,13 @@
-export interface TodoReducerState {
+export interface TodoItem {
+  id: string;
+  title: string;
+  description: string;
+  dueDate: string;
+  category: string;
+  completed: boolean;
+}
+
+export interface TodoState {
+  todos: TodoItem[];
   status: string;
 }
