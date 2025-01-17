743d8024f2a2852f9c24abd8d609ea2bb4694c88
Talis Lazdins <lazdinst@gmail.com>
2025-01-17 17:08:12 -0500

    added update todo service

--- server/src/services/todo.service.ts
+++ server/src/services/todo.service.ts
@@ -5,8 +5,18 @@ const todos: Todo[] = [];
 export const getTodos = (): Todo[] => todos;
 
 export const createTodo = (todo: Todo): Todo => {
+  //TODO:  uuid for this todo should be generated here
   todos.push(todo);
   return todo;
+};
+
+export const updateTodo = (id: string, updatedTodo: Todo): boolean => {
+  const index = todos.findIndex((todo) => todo.id === id);
+  if (index > -1) {
+    todos[index] = updatedTodo;
+    return true;
+  }
+  return false;
 };
 
 export const deleteTodo = (id: string): boolean => {
