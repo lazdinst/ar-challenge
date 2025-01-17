df33d86e0fd9c66d3921cbf7944ab53a41ef7920
Talis Lazdins <lazdinst@gmail.com>
2025-01-17 17:09:37 -0500

    added udpateTodocontroller to routes

--- server/src/routes/todo.routes.ts
+++ server/src/routes/todo.routes.ts
@@ -2,13 +2,15 @@ import express from "express";
 import {
   getTodosController,
   createTodoController,
+  updateTodoController,
   deleteTodoController,
-} from "../controllers/todo.controller";
+} from "../controllers";
 
 const router = express.Router();
 
 router.get("/", getTodosController);
 router.post("/", createTodoController);
+router.put("/", updateTodoController);
 router.delete("/:id", deleteTodoController);
 
 export default router;
