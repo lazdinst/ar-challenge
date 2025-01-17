c23439e01f2655b3987b44a1b0be526befcb844b
Talis Lazdins <lazdinst@gmail.com>
2025-01-17 17:10:10 -0500

    added todo controller for updating todo

--- server/src/controllers/todo.controller.ts
+++ server/src/controllers/todo.controller.ts
@@ -1,17 +1,35 @@
 import { Request, Response } from "express";
-import { getTodos, createTodo, deleteTodo } from "../services/todo.service";
+import {
+  getTodos,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+} from "../services/todo.service";
 
-export const getTodosController = (req: Request, res: Response) => {
+export const getTodosController = (req: Request, res: Response): void => {
   const todos = getTodos();
   res.status(200).json(todos);
+  return;
 };
 
-export const createTodoController = (req: Request, res: Response) => {
+export const createTodoController = (req: Request, res: Response): void => {
   const todo = createTodo(req.body);
   res.status(201).json(todo);
+  return;
 };
 
-export const deleteTodoController = (req: Request, res: Response) => {
+export const updateTodoController = (req: Request, res: Response): void => {
+  const { id } = req.params;
+  const success = updateTodo(id, req.body);
+  if (success) {
+    res.status(204).send();
+  } else {
+    res.status(404).json({ error: "Todo not found" });
+  }
+  return;
+};
+
+export const deleteTodoController = (req: Request, res: Response): void => {
   const { id } = req.params;
   const success = deleteTodo(id);
   if (success) {
@@ -19,4 +37,5 @@ export const deleteTodoController = (req: Request, res: Response) => {
   } else {
     res.status(404).json({ error: "Todo not found" });
   }
+  return;
 };
