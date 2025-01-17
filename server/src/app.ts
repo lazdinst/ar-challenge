59560900af1ee3180899573f8aeea7ba1ba56593
Talis Lazdins <lazdinst@gmail.com>
2025-01-17 17:13:25 -0500

    added routes to server app and added error handler

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,15 @@
 import express from "express";
-import { todoRoutes } from "./routes";
+import { todoRoutes, categoryRoutes } from "./routes";
+import { errorHandler } from "./utils";
 
 const app = express();
 app.use(express.json());
+app.get("/status", (req, res) => {
+  res.status(200).json({ message: "Server is running" });
+});
+
 app.use("/api/todos", todoRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use(errorHandler);
 
 export default app;
