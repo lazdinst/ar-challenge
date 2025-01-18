acb712a1839e4dfd131f5143d92d1e2e789c9159
lazdinst <lazdinst@gmail.com>
2025-01-18 08:46:05 -0500

    Added cors and created some dummy todos

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { RootState, useAppDispatch } from "../redux";
+import { fetchTodosThunk } from "../redux/slices/todo";
 
 const Home: React.FC = () => {
-  return <>home</>;
+  const dispatch = useAppDispatch();
+  const todos = useSelector((state: RootState) => state.todo.todos);
+  const loading = useSelector((state: RootState) => state.todo.loading);
+  const error = useSelector((state: RootState) => state.todo.error);
+
+  useEffect(() => {
+    dispatch(fetchTodosThunk());
+  }, [dispatch]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <h3>{todo.title}</h3>
+            <p>{todo.description}</p>
+            <p>{todo.category}</p>
+            <p>Due: {todo.dueDate}</p>
+            <p>Status: {todo.completed ? "Completed" : "Incomplete"}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
