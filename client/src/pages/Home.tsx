48c0d67ac701fa3b353fcd094b7cb2eb806249fa
lazdinst <lazdinst@gmail.com>
2025-01-18 20:00:15 -0500

    added the modal manager to home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,27 @@
 import React from "react";
-import { AddTodoContainer, TodoListManager } from "../containers";
+import {
+  AddTodoContainer,
+  TodoListManager,
+  EditTodoModal,
+} from "../containers";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux";
 
 const Home: React.FC = () => {
+  const modals = useSelector((state: RootState) => state.ui.modals);
+
   return (
     <div>
       <h1>Todo List</h1>
       <AddTodoContainer />
       <TodoListManager />
+      {Object.keys(modals).map((modalId) => {
+        if (modalId.startsWith("editTodo-")) {
+          const todoId = modalId.replace("editTodo-", "");
+          return <EditTodoModal key={modalId} todoId={todoId} />;
+        }
+        return null;
+      })}
     </div>
   );
 };
