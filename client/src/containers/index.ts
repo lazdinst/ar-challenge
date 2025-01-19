fbf612f10e6ea22aeb00bf5567573cdb5a3d33f6
lazdinst <lazdinst@gmail.com>
2025-01-18 19:08:17 -0500

    integrated the modal and edit todo containers

--- client/src/containers/index.ts
+++ client/src/containers/index.ts
@@ -1,2 +1,5 @@
 export { default as AddTodoContainer } from "./AddTodoContainer";
 export { default as TodoListContainer } from "./TodoListContainer";
+export { default as EditTodoContainer } from "./EditTodoContainer";
+export { default as ModalContainer } from "./ModalContainer";
+export { default as EditTodoModal } from "./EditTodoModal";
